21b5ed1ce117cac13f4d56858c22837bbfb38545
errabi ayoub <errabi.ayoub@gmail.com>
2024-06-29 18:59:06 +0100

    update layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Page() {
   return (
-    <h1 className="text-lg text-white">Liveblocks Figma Clone</h1>
+    <div className="h-[100vh] w-full flex justify-center items-center text-center">
+      <h1 className="text-lg text-white">Liveblocks Figma Clone</h1>
+    </div>
   );
 }
