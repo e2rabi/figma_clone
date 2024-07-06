be7cf4275ca6de6dc80618d668343a30c3dd8b10
errabi ayoub <errabi.ayoub@gmail.com>
2024-07-06 18:38:14 +0100

    setup live cursor component part 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+"use client"
+import Live from "@/components/Live";
 
 export default function Page() {
   return (
     <div className="h-[100vh] w-full flex justify-center items-center text-center">
       <h1 className="text-lg text-white">Liveblocks Figma Clone</h1>
+      <Live />
     </div>
   );
 }
